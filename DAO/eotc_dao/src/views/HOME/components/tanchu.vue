ff56646ebd0814841d316f7293b5baa7dc17e510
吴鸿儒 <852383879@qq.com>
2022-09-20 16:05:21 +0800

    postcss

---
+++ DAO/eotc_dao/src/views/HOME/components/tanchu.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="vanpopup1">
+    <van-popup v-model="temp_app" :overlay="false"   position="left" :style="{ height: '100%'  , width: '90%'}" >
+    <ul class="uls1" >
+        <li v-for="item in etot" :key="item.index">{{item.vaule}} <van-icon name="guide-o" /></li>
+    </ul>
+    </van-popup>
+  </div>
+</template>
+
+<script>
+export default {
+    
+  props: ['show'],
+
+    data(){
+        return{
+        temp_app:false,
+         etot:[
+            {vaule:'国际化',},
+            {vaule:'标准化'},
+            {vaule:'国际化2'},
+            {vaule:'国际化3'},
+            {vaule:'国际化4'},
+            {vaule:'国际化5'},
+            {vaule:'国际化6'},
+            {vaule:'国际化7'},
+            {vaule:'国际化8'},
+            {vaule:'国际9'},
+            {vaule:'国际化0'},
+
+         ]   
+
+        }
+    },
+    watch: {
+      show:{
+        
+        // this.$emit('temp_app', val);
+        handler: function(val) {
+        this.temp_app = val
+    },
+    immediate: true,
+    deep:true
+      },
+      
+    },
+
+//     deep:true,
+//     immediate:true
+//   },
+    methods:{
+        // ww1(){
+        //     this.shows=this.show
+        // }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- DAO/eotc_dao/src/views/HOME/home.vue
+++ DAO/eotc_dao/src/views/HOME/home.vue
@@ -1,36 +1,89 @@
 <template>
-  <div class="home">
-    <header>
-    <van-row class="header">
-  <van-col span="8"><van-icon name="bars" color="white" /></van-col>
-  <van-col span="8"><img src="./assets/image/eotc.png" alt=""></van-col>
-  <van-col span="8" ><van-icon name="manager-o"  color="white"/></van-col>
-</van-row>
-    </header>
-    <main></main>
-    <footer></footer>
+  <div>
+    <van-row class="home">
+      <van-col span="3" class="header">
+        <van-row>
+     
+          <van-col @click="chufa" span="2"
+            ><van-icon :name="shouw ? 'cross' : 'bars'" color="white" />
+          </van-col>
+          <van-col class="vancol2" span="20"
+            ><img src="./assets/image/eotc.png" alt=""
+          /></van-col>
+          <van-col span="2"
+            ><van-icon name="manager-o" color="white"
+          /></van-col>
+        </van-row>
+      </van-col>
+
+      <van-col span="11" class="main1">
+        <main>
+          <van-row class="rowa"> 
+            <van-col span="12" class="imgs2">
+              <van-image class="vanimg"
+                width="100"
+                height="100"
+                src="https://img01.yzcdn.cn/vant/cat.jpeg"
+              />
+            </van-col>
+            <van-col style="width:100%" span="4">
+              <h2 class="h2">{{daojn}}</h2>
+            </van-col>
+            <van-col class="vancol5" span="4" style="width:100%">
+             <p class="pp1">{{dao1}}</p>
+            </van-col>
+            <van-col span="4" class="vanhh">
+              <van-button type="info"  round size="normal">创建提案</van-button>
+            </van-col>
+          </van-row>
+          <Tanchu-vue :show="shouw"></Tanchu-vue>
+        </main>
+      </van-col>
+      <van-col span="8" class="main2">
+        <div class="newtian"></div>
+      </van-col>
+      <van-col span="2"></van-col>
+    </van-row>
   </div>
 </template>
 
 <script>
+import TanchuVue from "./components/tanchu.vue";
 export default {
-name:'home',
-  data(){
-    return{
-
-    }
-  }
-}
+  components: { TanchuVue },
+  name: "home",
+  data() {
+    return {
+      shouw: false,
+      daojn:'EOTC DAO',
+      dao1:'围绕EOTC所有去中去化业务建立的团队 允许任何成员做出决定，参加治理'
+    };
+  },
+  // mounted(){
+  //   console.log(1111111)
+  //   this.shouw=false
+  // },
+  methods: {
+    chufa() {
+      this.shouw = !this.shouw;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .home{
-    background: #070A0E;
-    height: 100vh;
-    width: 100%;
-  }
-  .header{
-    display: flex;
-
-  }
+.home {
+  background: #070a0e;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+}
+.header {
+  width: 100%;
+ 
+}
+.vancol2 {
+  text-align: center;
+}
 </style>
